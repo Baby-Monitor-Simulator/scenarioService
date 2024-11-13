9b528752b782b7b10cfe60ba858d6421c5111ca8
Antwan <99661069+antwan38@users.noreply.github.com>
2024-11-13 15:27:23 +0100

    halve mongodb implementatie

---
+++ src/main/java/com/babymonitor/scenario/config/MongoClientConfig.java
@@ -0,0 +1,35 @@
+package com.babymonitor.scenario.config;
+
+import com.mongodb.ConnectionString;
+import com.mongodb.MongoClientSettings;
+import com.mongodb.MongoException;
+import com.mongodb.ServerApi;
+import com.mongodb.ServerApiVersion;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoDatabase;
+import org.bson.Document;
+
+public class MongoClientConfig {
+    public static void connect() {
+        String connectionString = "mongodb+srv://babymonitor:H7427LwrAb8Ug@babymonitor.xgjmk.mongodb.net/?retryWrites=true&w=majority&appName=babymonitor";
+        ServerApi serverApi = ServerApi.builder()
+                .version(ServerApiVersion.V1)
+                .build();
+        MongoClientSettings settings = MongoClientSettings.builder()
+                .applyConnectionString(new ConnectionString(connectionString))
+                .serverApi(serverApi)
+                .build();
+        // Create a new client and connect to the server
+        try (MongoClient mongoClient = MongoClients.create(settings)) {
+            try {
+                // Send a ping to confirm a successful connection
+                MongoDatabase database = mongoClient.getDatabase("admin");
+                database.runCommand(new Document("ping", 1));
+                System.out.println("Pinged your deployment. You successfully connected to MongoDB!");
+            } catch (MongoException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
